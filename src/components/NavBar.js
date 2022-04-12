79d216f9e6f90f23c59161dc7f2adee4955f80c3
nicorossi19 <nicolas.grossi@hotmail.com>
2022-04-12 11:50:05 -0300

    JPEntrenadorPersonal+Rossi-desafio2

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className='Navbar'>
+        <h2 className='Logo'><a href='#'>JP Entrenador Personal</a></h2>
+        <div className='ProductsAndLogin'>
+          <ul className='NavbarListProducts'>
+            <li><a href='#'>Productos</a></li>
+            <li><a href='#'>Ofertas</a></li>
+          </ul>
+          <ul className='NavbarListLog'>
+            <li><a className='Signin' href='#'>Inicia sesión</a></li>
+            <li><a className='Signup' href='#'>Regístrate</a></li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
