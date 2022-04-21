2ebe30f0aac1c61f17263345fc55135bb493d5c0
nicorossi19 <nicolas.grossi@hotmail.com>
2022-04-21 03:42:40 -0300

    JPEntrenadorPersonal+Rossi-Desafio4

--- src/components/list-container/ItemCount.js
+++ src/components/list-container/ItemCount.js
@@ -8,20 +8,31 @@ const ItemCount = () => {
 
     const resta = () => item > 0 ? setItem(item - 1) : false;
     const suma = () => setItem(item + 1);
+
+    // useState para que salte una alerta cuando se intenta añadir al carrito 0 items
+    const [itemCount, setItemCount] = useState();
     
     return (
     <>
-        <div className='flex-col justify-center mt-20'>
-            <div className='bg-gray-200 h-52 w-96 mx-auto rounded-2xl flex items-center justify-center text-xl'>
-                <div className='flex justify-between items-center bg-slate-100 border-indigo-500 border-2 h-10 w-80 rounded-lg overflow-hidden'>
+        <div className='flex-col items-center h-96 mt-20'>
+            <div className='bg-gray-200 h-52 w-96 mx-auto rounded-2xl'>
+                <div className='py-8'>
+                    <h1 className='text-center text-xl'>Artículos</h1>
+                </div>
+                <div className='flex justify-between items-center bg-slate-100 border-indigo-500 border-2 h-10 w-80 rounded-lg overflow-hidden mx-auto'>
                     <button className='h-10 w-20 transition-colors hover:bg-indigo-500' onClick={resta}><GrSubtract className='mx-auto' /></button>
                     <h1>{item}</h1>
                     <button className='h-10 w-20 transition-colors hover:bg-indigo-500' onClick={suma}><GrAdd className='mx-auto' /></button>
                 </div>
             </div>
-            <button className='h-12 w-96 mt-5 bg-indigo-500 border-slate-100 border-2 mx-auto rounded-lg flex items-center justify-center transition-colors hover:bg-indigo-600'>
+            <button className='h-12 w-96 mt-5 bg-indigo-500 border-slate-100 border-2 mx-auto rounded-lg flex items-center justify-center transition-colors hover:bg-indigo-600' onClick={() => item === 0 ? setItemCount(true) : setItemCount(false)}>
                 <h3 className='text-xl w-auto h-auto text-slate-100'>Agregar al carrito</h3>
             </button>
+            {
+                itemCount ?
+                    <p className='text-red-600 text-center font-semibold mt-5'>Debes seleccionar al menos uno para añadir al carrito.</p> :
+                    null
+            }
         </div>
     </>
     )
